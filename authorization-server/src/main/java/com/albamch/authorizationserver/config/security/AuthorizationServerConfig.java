7d577c3af7b5739d6848215e54727970c6c2dbca
artfern95 <albamolerachacon@gmail.com>
2022-01-19 21:49:17 +0100

    clave rsa para auth service

--- authorization-server/src/main/java/com/albamch/authorizationserver/config/security/AuthorizationServerConfig.java
+++ authorization-server/src/main/java/com/albamch/authorizationserver/config/security/AuthorizationServerConfig.java
@@ -3,7 +3,7 @@ package com.albamch.authorizationserver.config.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
-import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationManager;8
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
